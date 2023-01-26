96462006b34adf30610f221992cee42402355e02
uytpv <traphucvinhuy012022@gmail.com>
2023-01-26 16:11:37 +0200

    from Mac

--- app/src/main/java/com/example/numerologycalculator/MainActivity.java
+++ app/src/main/java/com/example/numerologycalculator/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.numerologycalculator;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +14,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button loginButton = (Button) findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText emailEditText = (EditText) findViewById(R.id.emailEditText);
+                EditText passwordEditText = (EditText) findViewById(R.id.passwordEditText);
+
+                String email = String.valueOf(emailEditText.getText());
+                String password = String.valueOf(passwordEditText.getText());
+
+                if(email.isEmpty() || password.isEmpty()){
+                    emailEditText.setError("Your email or password is not correct");
+                } else {
+                    if(email == "admin@gmail.com" && password == "abc@123"){
+                        Intent startIntent = new Intent(getApplicationContext(), CustomerListActivity.class);
+                        startActivity(startIntent);
+                    } else {
+                        emailEditText.setError("Your email or password is not correct");
+                    }
+                }
+
+
+            }
+        });
     }
 }
