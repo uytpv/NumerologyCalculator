85b43e8157a6028c928100c0b6702ad3f105f376
amun1303 <pigbanguy@gmail.com>
2023-01-27 11:40:57 +0200

    Done Login

--- app/src/main/java/com/example/numerologycalculator/MainActivity.java
+++ app/src/main/java/com/example/numerologycalculator/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -21,22 +22,28 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 EditText emailEditText = (EditText) findViewById(R.id.emailEditText);
                 EditText passwordEditText = (EditText) findViewById(R.id.passwordEditText);
+                TextView emailErrorTextView = (TextView) findViewById(R.id.emailErrorTextView);
+                TextView passwordErrorTextView = (TextView) findViewById(R.id.passwordErrorTextView);
+
 
                 String email = String.valueOf(emailEditText.getText());
                 String password = String.valueOf(passwordEditText.getText());
 
-                if(email.isEmpty() || password.isEmpty()){
-                    emailEditText.setError("Your email or password is not correct");
+                if (email.isEmpty()) {
+                    emailErrorTextView.setText("Required");
+                } else if (password.isEmpty()) {
+                    passwordErrorTextView.setText("Required");
                 } else {
-                    if(email == "admin@gmail.com" && password == "abc@123"){
+                    emailErrorTextView.setText("");
+                    passwordErrorTextView.setText("");
+                    if (email.equals("uytpv@gmail.com") && password.equals("abc@123")) {
                         Intent startIntent = new Intent(getApplicationContext(), CustomerListActivity.class);
                         startActivity(startIntent);
                     } else {
                         emailEditText.setError("Your email or password is not correct");
+                        passwordEditText.setError("Your email or password is not correct");
                     }
                 }
-
-
             }
         });
     }
